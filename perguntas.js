f5f3b4600c7997019fee47eeda7b44b14de16fef
isa-governo-aluna <00001109023959SP@al.educacao.sp.gov.br>
2024-11-18 11:41:48 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'História',
+    'Quanto tempo durou a guerra dos cem anos?',
+    '116 anos'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'História',
+    'É correto afirmar que Tiradentes foi',
+    'Líder da Inconfidência Mineira'
+)
